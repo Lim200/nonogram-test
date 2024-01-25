1d7002459e353bc74f1ebae340f03c7ffb333788
Lim200 <112889518+Lim200@users.noreply.github.com>
2024-01-25 22:42:20 +0300

    feat: add function random

--- nonograms/js/index.js
+++ nonograms/js/index.js
@@ -1,20 +1,31 @@
-import { nonogramDataObj, nonogramGameField } from "./layout.js";
+import { nonogramDataObj, nonogramGameFieldObj } from './layout.js';
 
-const nonogramData = nonogramDataObj.letterN;
+const nameTemplates = ['grid', 'person', 'rocket', 'letterN', 'face'];
 
-const nonogramContainer = document.getElementById("nonogram");
+function generateTemplates() {
+  const randomIndex = Math.floor(Math.random() * nameTemplates.length);
+  console.log(nameTemplates[randomIndex]);
+  return nameTemplates[randomIndex];
+}
+
+let templatesRandom = generateTemplates();
+const nonogramData = nonogramDataObj[templatesRandom];
+
+const nonogramGameField = nonogramGameFieldObj[templatesRandom];
+
+const nonogramContainer = document.getElementById('nonogram');
 let cell;
 // let cellsGameField = [];
 
 function createNonogramTable(data) {
-  const table = document.createElement("table");
+  const table = document.createElement('table');
 
   const firstGameFieldRow = data.length - nonogramGameField.length;
   const firstGameFieldCol = data[0].length - nonogramGameField[0].length;
 
   // table.classList.add("nonogramTable");
   for (let row = 0; row < data.length; row++) {
-    const tr = document.createElement("tr");
+    const tr = document.createElement('tr');
     for (let col = 0; col < data[row].length; col++) {
       // console.log(
       //   "data[row][col]",
@@ -23,36 +34,36 @@ function createNonogramTable(data) {
       //   typeof +data[row][col]
       // );
       let elemTable = data[row][col];
-      let cellType = elemTable === "" || !isNaN(elemTable) ? "th" : "td";
+      let cellType = elemTable === '' || !isNaN(elemTable) ? 'th' : 'td';
       cell = document.createElement(cellType);
       cell.textContent = data[row][col];
 
-      if (cell.textContent === "a" || cell.textContent === "x") {
-        cell.textContent = "";
-        cell.className = "gameField";
-      } else if (cell.textContent === "") {
-        cell.className = "fieldLeft";
+      if (cell.textContent === 'a' || cell.textContent === 'x') {
+        cell.textContent = '';
+        cell.className = 'gameField';
+      } else if (cell.textContent === '') {
+        cell.className = 'fieldLeft';
       }
 
       if (col === firstGameFieldCol - 1) {
-        cell.style.borderRight = "3px solid black";
+        cell.style.borderRight = '3px solid black';
       }
       if (row === firstGameFieldRow - 1) {
-        cell.style.borderBottom = "3px solid black";
+        cell.style.borderBottom = '3px solid black';
       }
 
-      if (cell.classList.contains("gameField")) {
+      if (cell.classList.contains('gameField')) {
         if ((row - firstGameFieldRow) % 5 === 0) {
-          cell.style.borderTop = "3px solid black";
+          cell.style.borderTop = '3px solid black';
         }
         if ((col - firstGameFieldCol) % 5 === 0) {
-          cell.style.borderLeft = "3px solid black";
+          cell.style.borderLeft = '3px solid black';
         }
       }
 
-      if (cell.classList.contains("gameField")) {
-        cell.addEventListener("click", leftClick);
-        cell.addEventListener("contextmenu", rightClick);
+      if (cell.classList.contains('gameField')) {
+        cell.addEventListener('click', leftClick);
+        cell.addEventListener('contextmenu', rightClick);
       }
 
       tr.appendChild(cell);
@@ -66,39 +77,39 @@ function createNonogramTable(data) {
 nonogramContainer.appendChild(createNonogramTable(nonogramData));
 
 function leftClick(event) {
-  if (!this.classList.contains("blackField")) {
-    this.classList.add("blackField");
-    this.classList.remove("crossedField");
+  if (!this.classList.contains('blackField')) {
+    this.classList.add('blackField');
+    this.classList.remove('crossedField');
   } else {
-    this.classList.remove("blackField");
-    this.classList.remove("crossedField");
+    this.classList.remove('blackField');
+    this.classList.remove('crossedField');
   }
   checkFill();
 }
 
 function rightClick(event) {
   event.preventDefault();
-  if (!this.classList.contains("crossedField")) {
-    this.classList.add("crossedField");
-    this.classList.remove("blackField");
+  if (!this.classList.contains('crossedField')) {
+    this.classList.add('crossedField');
+    this.classList.remove('blackField');
   } else {
-    this.classList.remove("crossedField");
+    this.classList.remove('crossedField');
   }
   checkFill();
 }
 
 function checkFill() {
-  const tableRows = document.querySelectorAll("#nonogram table tr");
+  const tableRows = document.querySelectorAll('#nonogram table tr');
   let cellsGameField = [];
 
   for (let rowIndex = 1; rowIndex < tableRows.length; rowIndex++) {
-    const cells = tableRows[rowIndex].querySelectorAll("td");
+    const cells = tableRows[rowIndex].querySelectorAll('td');
     let rowCourantGameField = [];
 
     for (let cellIndex = 0; cellIndex < cells.length; cellIndex++) {
       const cell = cells[cellIndex];
       rowCourantGameField.push(
-        cell.classList.contains("blackField") ? "a" : "x"
+        cell.classList.contains('blackField') ? 'a' : 'x'
       );
     }
     if (rowCourantGameField.length !== 0) {
@@ -106,7 +117,7 @@ function checkFill() {
     }
   }
 
-  // console.log("cellsGameField", cellsGameField);
+  // console.log('cellsGameField', cellsGameField);
 
   let isCorrect = true;
   for (let rowIndex = 0; rowIndex < cellsGameField.length; rowIndex++) {
@@ -120,15 +131,17 @@ function checkFill() {
         nonogramGameField[rowIndex][colIndex]
       ) {
         isCorrect = false;
+        // console.log('Break1');
         break;
       }
     }
     if (!isCorrect) {
+      // console.log('Break2');
       break;
     }
   }
 
   if (isCorrect) {
-    console.log("Great! You have solved the nonogram!");
+    console.log('Great! You have solved the nonogram!');
   }
 }
